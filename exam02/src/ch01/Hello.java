f22231ac045f018499981b76a0c42c50d0e50031
Mingyu Kim <mkkim1196@gmail.com>
2023-03-20 15:34:28 +0900

    마지막?

--- exam02/src/ch01/Hello.java
+++ exam02/src/ch01/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 			System.out.println("Hello World1");
 			System.out.println("Hello World2");
+			System.out.println("Hello World2");
 	}
 
 }
